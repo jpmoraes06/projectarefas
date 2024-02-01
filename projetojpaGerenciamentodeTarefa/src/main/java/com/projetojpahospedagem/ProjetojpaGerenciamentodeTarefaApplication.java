e29d8c21f0e24278667efa2dcd94fe31b50aa3e5
jpmoraes06 <150357838+jpmoraes06@users.noreply.github.com>
2024-02-01 16:21:02 -0300

    Add files via upload

---
+++ projetojpaGerenciamentodeTarefa/src/main/java/com/projetojpahospedagem/ProjetojpaGerenciamentodeTarefaApplication.java
@@ -0,0 +1,13 @@
+package com.projetojpahospedagem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetojpaGerenciamentodeTarefaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetojpaGerenciamentodeTarefaApplication.class, args);
+	}
+
+}
---
+++ projetojpaGerenciamentodeTarefa/src/test/java/com/projetojpahospedagem/ProjetojpaGerenciamentodeTarefaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetojpahospedagem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetojpaGerenciamentodeTarefaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
